cdac92dbac88d8c87156d5522171945526d5e114
Ryan DeCook <ryandecook@gmail.com>
2017-11-27 09:32:42 -0600

    add tasks get route

---
+++ server/routes/tasks.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+var pool = require('../modules/pool');
+
+
+// begin GET todo
+router.get('/', function(req,res){
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query('SELECT * FROM tasks ORDER BY id;', function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else{
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+});//end GET todo
+
+
+
+
+
+
+
+
+module.exports = router;
